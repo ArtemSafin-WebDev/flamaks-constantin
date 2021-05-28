270b4f622e6329b76b533baa174368cfe79070a7
Konstantin Voronin <konstantinweb89@gmail.com>
2021-05-28 11:20:41 +0300

    perfect desktop html-layout (no effects, no modals)

--- src/js-scripts/app.js
+++ src/js-scripts/app.js
@@ -9,7 +9,7 @@ const doPageMainSlider = function(selector) {
     if ($pageMainSlider.length === 0) return false;
 
     const sliderOptions = {
-        dots: false,
+        dots: true,
         arrows: true,
         infinite: true,
         speed: 300,
@@ -17,9 +17,19 @@ const doPageMainSlider = function(selector) {
         slidesToScroll: 1,
         fade: true,
         autoplay: true,
-        autoplaySpeed:5000,
+        autoplaySpeed:4000,
         pauseOnHover:false,
+        pauseOnFocus:false,
         lazyLoad: 'ondemand',
+        responsive: [
+            {
+                breakpoint: 501,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            },
+        ]
     };
 
     $pageMainSlider.each(function (idx, item) {
@@ -40,13 +50,11 @@ const doPageMainSlider = function(selector) {
         const newOptions = Object.assign(sliderOptions, {
             prevArrow: $prevArrow,
             nextArrow: $nextArrow,
-        })
+        });
 
         $sliderCurrentPage.html( numToStr(1) );
         $sliderAllPage.html( numToStr(countSlides) );
         $slider.slick(newOptions);
-
-
 
         $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
             $mainSliderImagesSlides.removeClass('slide-active');
@@ -56,6 +64,139 @@ const doPageMainSlider = function(selector) {
         });
     });
 };
+
+const doNewsMainSlider = function () {
+
+    const $newsMainSliders =  $('.news-main-slider');;
+    if ($newsMainSliders.length === 0) return false;
+
+    const sliderOptions = {
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed:5000,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+        lazyLoad: 'ondemand',
+    };
+
+    $newsMainSliders.each(function (idx, item) {
+        const $slider = $(item);
+        const $sliderWrap = $slider.closest('.slider-wrap');
+        const $prevArrow = $sliderWrap.find('.btn-slider.prev');
+        const $nextArrow = $sliderWrap.find('.btn-slider.next');
+        const countSlides = $slider.find('.slide-item').length;
+
+        const $sliderCurrentPage = $sliderWrap.find('.slider-counter-current');
+        const $sliderAllPage = $sliderWrap.find('.slider-counter-all');
+
+        const $mainSliderImages = $sliderWrap.find('.main-slider-images');
+        const $mainSliderImagesSlides = $mainSliderImages.find('.main-slider-images-item');
+        $mainSliderImagesSlides.eq(0).addClass('slide-active');
+
+
+        const newOptions = Object.assign(sliderOptions, {
+            prevArrow: $prevArrow,
+            nextArrow: $nextArrow,
+        });
+
+        $sliderCurrentPage.html(1);
+        $sliderAllPage.html( countSlides);
+        $slider.slick(newOptions);
+
+        $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            $mainSliderImagesSlides.removeClass('slide-active');
+            $mainSliderImagesSlides.eq(nextSlide).addClass('slide-active');
+            $sliderCurrentPage.html( nextSlide + 1);
+
+        });
+    });
+
+
+
+}
+
+const doSideBlockSlider = function () {
+    const $sideBlockSliders = $('.side-block-slider');
+
+    if ($sideBlockSliders.length === 0) return false;
+
+    const sliderOptions = {
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 450,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplay: false,
+        autoplaySpeed:4000,
+        pauseOnHover:true,
+        pauseOnFocus:true,
+        lazyLoad: 'ondemand',
+    };
+
+    $sideBlockSliders.each(function (idx, item) {
+        const $slider = $(item);
+        const $sliderWrap = $slider.closest('.slider-wrap');
+        const $prevArrow = $sliderWrap.find('.btn-slider.prev');
+        const $nextArrow = $sliderWrap.find('.btn-slider.next');
+
+        const newOptions = Object.assign(sliderOptions, {
+            prevArrow: $prevArrow,
+            nextArrow: $nextArrow,
+        });
+        $slider.slick(newOptions);
+    });
+}
+
+const doNewsColumnsSlider = function () {
+    const $newsColumnsSlider = $('.news-columns-slider');
+
+    if ($newsColumnsSlider.length === 0) return false;
+
+    const sliderOptions = {
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 450,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplay: false,
+        autoplaySpeed:4000,
+        pauseOnHover:true,
+        pauseOnFocus:true,
+        lazyLoad: 'ondemand',
+    };
+
+    $newsColumnsSlider.each(function (idx, item) {
+        const $slider = $(item);
+        const $sliderWrap = $slider.closest('.slider-wrap');
+        const $prevArrow = $sliderWrap.find('.btn-slider.prev');
+        const $nextArrow = $sliderWrap.find('.btn-slider.next');
+
+        const newOptions = Object.assign(sliderOptions, {
+            prevArrow: $prevArrow,
+            nextArrow: $nextArrow,
+        });
+        $slider.slick(newOptions);
+    });
+}
+
+
+
+
+
+
+
+
+
 
 const doAccordionApp = function () {
     const $appAccordion = $('.app-accordion');
@@ -84,94 +225,121 @@ const doAccordionApp = function () {
     }
 };
 
-
 const doTechSlider = function () {
     const $techSlider = $('.tech-slider');
+
+    function fireTechSlider($thisTab, $techSliderTabs, $slider) {
+
+        if (($slider).hasClass('tech-animated')) return false;
+        if ( $thisTab.hasClass('tech-slider-tab-active') ) return false;
+
+        $techSliderTabs.removeClass('tech-slider-tab-active');
+        $thisTab.addClass('tech-slider-tab-active');
+
+        const $techSliderMediaCover = $slider.find('.tech-slider-media .tech-slider-media-cover');
+        const width = $techSliderMediaCover.width();
+
+        const $techSectionTitle =  $slider.find('.tech-slider-media  .tech-section-title');
+        const $techSectionDescriptionContent =  $slider.find('.tech-slider-media  .tech-section-description');
+
+        /* получить контент таба */
+
+        const imgHtml = $thisTab.find('.tech-slider-tab-img-container').html();
+        const titleHtml = $thisTab.find('.tech-slider-tab-title').html();
+        const descHtml = $thisTab.find('.tech-slider-tab-description').html();
+
+        /*подготовить контент для вывода*/
+
+        const prepImg = `
+                <div class="tech-section-cover-frame new">
+                    <div class="tech-section-cover-frame-wrap " style="width:${width}px">
+                      ${imgHtml}
+                    </div>
+                </div>
+            `;
+
+        const prepTitle = `
+                <div class="tech-section-title-content new">
+                    ${titleHtml}
+                </div>
+            `;
+        const prepDesc = `
+                <div class="tech-section-description-content new">
+                    ${descHtml}
+                </div>
+            `;
+
+        $techSliderMediaCover.append(prepImg);
+        $techSectionTitle.append(prepTitle);
+        $techSectionDescriptionContent.append(prepDesc);
+
+        const setTimeDelayStart = setTimeout(function () {
+            $slider
+                .addClass('tech-animated')
+                .addClass('tech-animated-start');
+            const setTimeDelayEnd = setTimeout(function () {
+                $slider
+                    .removeClass('tech-animated-start')
+                    .addClass('tech-animated-end');
+
+                const setTimeDelayFinish = setTimeout(function () {
+
+                    $slider.find(`
+                    .tech-section-title-content:not(.new), 
+                    .tech-section-description-content:not(.new), 
+                    .tech-section-cover-frame:not(.new)`).remove();
+
+                    $slider.find(`
+                    .tech-section-title-content, 
+                    .tech-section-description-content,  
+                    .tech-section-cover-frame`).removeClass('new');
+
+                    $slider
+                        .removeClass('tech-animated-end')
+                        .removeClass('tech-animated');
+
+                    clearTimeout(setTimeDelayStart);
+                    clearTimeout(setTimeDelayEnd);
+                    clearTimeout(setTimeDelayFinish);
+                }, 400)
+            }, 400)
+        }, 50);
+
+
+    }
+
+    function fireTechAccordion($thisTab, $techSliderTabs) {
+        $techSliderTabs.not($thisTab)
+            .removeClass('accordion-active')
+            .find('.tech-section-body').stop().slideUp(200);
+
+        if ($thisTab.hasClass('accordion-active')) {
+            $thisTab
+                .removeClass('accordion-active')
+                .find('.tech-section-body').stop().slideUp(200);
+        } else {
+            $thisTab
+                .addClass('accordion-active')
+                .find('.tech-section-body').stop().slideDown(200);
+        }
+    }
+
+
     $techSlider.each(function (idx, item) {
         const $slider = $(item);
 
         const $techSliderTabs = $slider.find('.tech-slider-tab');
 
+
+
         $techSliderTabs.on('click', ' .tech-section-head', function (evt) {
-
-            if (($slider).hasClass('tech-animated')) return false;
-
-
             const $thisTab = $(this).closest('.tech-slider-tab');
 
-            if ( $thisTab.hasClass('tech-slider-tab-active') ) return false;
-            $techSliderTabs.removeClass('tech-slider-tab-active');
-            $thisTab.addClass('tech-slider-tab-active');
-
-            const $techSliderMediaCover = $slider.find('.tech-slider-media .tech-slider-media-cover');
-            const width = $techSliderMediaCover.width();
-
-            const $techSectionTitle =  $slider.find('.tech-slider-media  .tech-section-title');
-            const $techSectionDescriptionContent =  $slider.find('.tech-slider-media  .tech-section-description');
-
-            /* получить контент таба */
-
-            const imgHtml = $thisTab.find('.tech-slider-tab-img-container').html();
-            const titleHtml = $thisTab.find('.tech-slider-tab-title').html();
-            const descHtml = $thisTab.find('.tech-slider-tab-description').html();
-
-            /*подготовить контент для вывода*/
-
-            const prepImg = `
-        <div class="tech-section-cover-frame new">
-            <div class="tech-section-cover-frame-wrap " style="width:${width}px">
-              ${imgHtml}
-            </div>
-        </div>
-    `;
-
-            const prepTitle = `
-        <div class="tech-section-title-content new">
-            ${titleHtml}
-        </div>
-    `;
-            const prepDesc = `
-        <div class="tech-section-description-content new">
-            ${descHtml}
-        </div>
-    `;
-
-
-            $techSliderMediaCover.append(prepImg);
-            $techSectionTitle.append(prepTitle);
-            $techSectionDescriptionContent.append(prepDesc);
-
-            const setTimeDelayStart = setTimeout(function () {
-                $slider
-                    .addClass('tech-animated')
-                    .addClass('tech-animated-start');
-                const setTimeDelayEnd = setTimeout(function () {
-                    $slider
-                        .removeClass('tech-animated-start')
-                        .addClass('tech-animated-end');
-
-                    const setTimeDelayFinish = setTimeout(function () {
-
-                        $slider.find(`
-                    .tech-section-title-content:not(.new), 
-                    .tech-section-description-content:not(.new), 
-                    .tech-section-cover-frame:not(.new)`).remove();
-
-                        $slider.find(`
-                    .tech-section-title-content, 
-                    .tech-section-description-content,  
-                    .tech-section-cover-frame`).removeClass('new');
-
-                        $slider
-                            .removeClass('tech-animated-end')
-                            .removeClass('tech-animated');
-
-                        clearTimeout(setTimeDelayStart);
-                        clearTimeout(setTimeDelayEnd);
-                        clearTimeout(setTimeDelayFinish);
-                    }, 400)
-                }, 400)
-            }, 50)
+            if ($(window).width() > 781) {
+                fireTechSlider($thisTab, $techSliderTabs, $slider)
+            } else {
+                fireTechAccordion($thisTab, $techSliderTabs)
+            }
 
         });
     });
@@ -229,29 +397,7 @@ const doElementFullHeight = function (selector, minMedia = 0) {
             $(item).height(winHeight);
         })
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -260,8 +406,12 @@ $(document).ready(function () {
     doAccordionApp();
     doTechSlider();
     doSliderTrust();
-    doUserSelect('.custom-select');
+    doSideBlockSlider();
+    doNewsColumnsSlider();
+    doNewsMainSlider();
 
+
+    doUserSelect('.custom-select');
     doElementFullHeight('.block-full-height')
 
 })
